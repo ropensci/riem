0470ecaa789bedede0008868bc8417169819b9fe
masalmon <maelle.salmon@yahoo.se>
2016-04-22 16:29:32 +0200

    adds function for getting stations in a network

---
+++ R/stations.R
@@ -0,0 +1,29 @@
+#' Function for getting stations of an ASOS network
+#'
+#' @importFrom dplyr select_ "%>%" tbl_df mutate_
+#' @importFrom lazyeval interp
+#'
+#' @parameter network network code, see riem_networks() for finding the code corresponding to a name.
+#' @return a data.frame (dplyr tbl_df) with the id, name, longitude (lon) and latitude (lat) of each station in the network.
+#' @export
+#'
+#' @examples
+#' riem_stations(network = "AE__ASOS")
+riem_stations <- function(network = NULL){
+
+  if(is.null(network)){
+    stop("Please provide a network code",
+         call. = FALSE)
+  }
+
+  link <- paste0("http://mesonet.agron.iastate.edu/json/network.php?network=",
+                 network)
+
+  content <- jsonlite::fromJSON(link)
+  tbl_df(content$stations) %>%
+    select_(quote(- combo)) %>%
+    mutate_(lon = interp(~ as.numeric(lon))) %>%
+    mutate_(lat = interp(~ as.numeric(lat)))
+}
+
+
--- R/utils.R
+++ R/utils.R
@@ -2,6 +2,7 @@ check_network <- function(network_code){
   codes <- riem_networks()$codes
 
   if(network_code %in% codes){
-    stop(paste(network_code, "is not the code of an ASOS network"))
+    stop(paste(network_code, "is not the code of an ASOS network"),
+         call. = FALSE)
   }
 }
