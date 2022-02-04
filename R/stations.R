24462498299c7f0a301490f097f76748b93876c9
Maëlle Salmon <maelle.salmon@yahoo.se>
2022-02-04 15:20:04 +0100

    more

--- R/stations.R
+++ R/stations.R
@@ -1,5 +1,7 @@
 #' Function for getting stations of an ASOS network
 #'
+#' @importFrom rlang %||%
+#'
 #' @param network A single network code, see riem_networks() for finding the code corresponding to a name.
 #' @return a data.frame (tibble tibble) with the id, name, longitude (lon) and latitude (lat) of each station in the network.
 #' @details You can see a map of stations in a network at \url{https://mesonet.agron.iastate.edu/request/download.phtml}.
@@ -11,32 +13,35 @@
 #' }
 riem_stations <- function(network = NULL){
 
-  if(is.null(network)){
-    stop("Please provide a network code",
-         call. = FALSE)
-  }
+  valid_network_code <- !is.null(network) && (network %in% riem_networks()$code)
+  if(!valid_network_code) error_invalid_network(network)
 
-  if(!(network %in% riem_networks()$code)){
-    stop(paste0(network,
-                " is not a valid network code. See riem_networks()"),
-         call. = FALSE) # nolint
-  }
+  resp <- sprintf("http://mesonet.agron.iastate.edu/api/1/network/%s.json", network) %>%
+    httr2::request() %>%
+    httr2::req_user_agent("riem (https://docs.ropensci.org/riem)") %>%
+    httr2::req_retry(max_tries = 3, max_seconds = 120) %>%
+    httr2::req_perform()
 
-  link <- paste0("http://mesonet.agron.iastate.edu/api/1/network/", network, ".json")
+  httr2::resp_check_status(resp)
 
-  resp <- httr::GET(link)
-  httr::stop_for_status(resp)
+  content <- httr2::resp_body_json(resp)
 
-  content <- jsonlite::fromJSON(
-    httr::content(
-      resp, as ="text"
-    )
-  )
+  results <- content[["data"]] %>%
+    purrr::map_df(function(x) tibble::as_tibble(purrr::compact(x)))
 
-  results <- tibble::as_tibble(content$data)
+
   results <- results[, !names(results) == "combo"]
-  results$lon <- as.numeric(results$longitude)
-  results$lat <- as.numeric(results$latitude)
+  results$longitude <- as.numeric(results$longitude)
+  results$latitude <- as.numeric(results$latitude)
 
   return(results)
+}
+
+error_invalid_network <- function(network) {
+  rlang::abort(
+    c(
+      x = sprintf("%s is an invalid network code.", as.character(network %||% "null")),
+      i = "See riem_networks() for valid codes."
+    )
+  )
 }
